cabec91437676d6aa339aafab36790ca8f510970
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-09-23 19:46:38 -0400

    Created Weather component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">Weather App</div>
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }
