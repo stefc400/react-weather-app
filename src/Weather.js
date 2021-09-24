4d5c1694ed273520f3188ddba7489275402c78f5
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-09-24 16:00:23 -0400

    Added html structure + border

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,42 @@
 import React from "react";
 import "./Weather.css";
 export default function Weather() {
-  return <div className="Weather">Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Paris</h1>
+      <ul>
+        <li>Wednesday 12:00</li>
+        <li>Clear sky</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="weather icon"
+          ></img>
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
