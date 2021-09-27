d0edb8300c6bde72639fc760b8a11f924fea7dc5
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-09-27 17:57:07 -0400

    Improved conditional rendering by adding a function

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,14 +5,23 @@ import ForecastDay from "./ForecastDay";
 export default function Forecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecastData(response.data.daily);
     setLoaded(true);
   }
 
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
+  function load() {
+    let apiKey = "4cee29ecbdcb246efdb3579f6d95b7f0";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   if (loaded) {
     return (
@@ -33,11 +42,7 @@ export default function Forecast(props) {
       </div>
     );
   } else {
-    let apiKey = "4cee29ecbdcb246efdb3579f6d95b7f0";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
     return null;
   }
 }
